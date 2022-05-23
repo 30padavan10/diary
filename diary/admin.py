0915fa47f82fc168cbb4d73602d11df36a17e810
30padavan10 <30padavan10@gmail.com>
2022-05-24 00:37:54 +0500

    add admin classes

--- diary/admin.py
+++ diary/admin.py
@@ -4,9 +4,27 @@ from django.contrib import admin
 from .models import Teacher, Student, School, SchoolClass, Grade, Subject, Lesson
 
 admin.site.register(Teacher)
-admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('fio', 'school_class',)
+
+#admin.site.register(Student)
+
+
+
 admin.site.register(School)
 admin.site.register(SchoolClass)
-admin.site.register(Grade)
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('lesson', 'student', 'grade')
+
+#admin.site.register(Grade)
 admin.site.register(Subject)
-admin.site.register(Lesson)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('lesson_date', 'teacher', 'school', 'school_class', 'subject')
+
+#admin.site.register(Lesson)
